d898a6d3a183980abc7c3f522a720ae0537bd807
Hyeonmo <gusah8504@gmail.com>
2021-11-15 18:11:18 +0900

    node

--- admin.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host : 'us-cdbr-east-04.cleardb.com', //호스트 주소 
-    port : '3306', //mysql port
-    user : 'bd84a717c2fce4',
-    password : '40a77de8',
-    database : 'heroku_9d21971c5dbc6fb'
-}
